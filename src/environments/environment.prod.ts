8d90e9fe3b36ec01e92ae3220ff997a83110dac3
Juan David Gomez <juan.dev1809@gmail.com>
2026-05-19 04:12:04 -0500

    Agregar environment.prod.ts (anon key es publica por diseno)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://huzetyrhffaoxoggeyop.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1emV0eXJoZmZhb3hvZ2dleW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzk0ODUsImV4cCI6MjA5NDc1NTQ4NX0.2vtPXVpgXGx2S5iyG8EWVLM1ldZEOBDwEKCKgqcDPnw',
+  formspreeUrl: 'https://formspree.io/f/mblgpnvb',
+  googleSheetsUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9gc0iLBVHnuWopuxijsTupBn_tvZ-B7D3b4WBVZ-meyKF61a8o24Qy1FQ5fxUYL7DaabkkAgILjac/pub?gid=421205038&single=true&output=csv',
+};
